584692130fbcce8a199caa141f8f0ba168ee66f7
Ciera DePauw <cieragrace18@gmail.com>
2022-10-29 17:26:44 -0600

    add backround image and beginning stages of formatting

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ var addRecipeButton.document.querySelector(".add-recipe-button")
 var letsCookButton.document.querySelector(".let's-cook-button")
 addRecipeButton.addEventListener('click', function)
 letsCookButton.addEventListener('click', function)
+
+va
+function addRecipeButton() {
+
+}
+
+function letsCookButton() {
+
+}
