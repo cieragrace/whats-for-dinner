dc269f42598efa55e492f3b1cc3195ca02335c34
Ciera DePauw <cieragrace18@gmail.com>
2022-10-30 10:45:19 -0600

    Add arrays to main.js

--- main.js
+++ main.js
@@ -3,7 +3,33 @@ var letsCookButton.document.querySelector(".let's-cook-button")
 addRecipeButton.addEventListener('click', function)
 letsCookButton.addEventListener('click', function)
 
-va
+var sides = ['Miso Glazed Carrots',
+'Coleslaw', 'Garden Salad', 'Crispy Potatoes',
+'Sweet Potato Tots', 'Coconut Rice', 'Caeser Salad', 'Shrimp Summer Rolls', 'Garlic Butter Rolls',
+'Hush Puppies'];
+var mains = ['Spaghetti and Meatballs',
+'Pineapple Chicken', 'Shakshuka', 'Thai Yellow Curry',
+'Bibimbap', 'Chicken Parmesean', 'Butternut Squash Soup', 'BBQ Chicken Burgers',
+'Ramen', 'Empanadas', 'Chicken Fried Rice', 'Sheet Pan Fajitas', 'Margarita Pizza'];
+var desserts  = ['Apple Pie',
+'Lemon Meringue Pie'
+'Black Forest Cake',
+'Banana Bread',
+'Peach Cobbler',
+'Cheesecake',
+'Funfetti Cake',
+'Baklava',
+'Flan',
+'Macarons',
+'Macaroons',
+'Chocolate Cupcakes',
+'Pavlova',
+'Pumpkin Pie',
+'Key Lime Pie',
+'Tart Tatin',
+'Croissants',
+'Eclairs'];
+
 function addRecipeButton() {
 
 }
@@ -11,3 +37,7 @@ function addRecipeButton() {
 function letsCookButton() {
 
 }
+
+
+//user selects meal type and let's cook letsCookButton
+//meal is pulled from array and displayed in box 2(hide svg)
